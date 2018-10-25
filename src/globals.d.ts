7d2b335ed9da6edc19bed476a81f936c042d7b5b
daishaoshuai <shdai@cisco.com>
2018-10-25 15:28:41 +0800

    tslint

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,4 +1,4 @@
+import './typings/bezier-js';
 import './typings/graham-scan-convex-hull.d.ts';
 import './typings/polygon-offset.d.ts';
 import './typings/polygon.d.ts';
-import './typings/bezier-js';
