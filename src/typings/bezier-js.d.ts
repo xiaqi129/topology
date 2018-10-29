38e4766aa5dea6427257f9721245e6e69e0ab0b5
daishaoshuai <shdai@cisco.com>
2018-10-29 15:17:06 +0800

    add typings

---
+++ src/typings/bezier-js.d.ts
@@ -0,0 +1,7 @@
+declare class Bezier {
+  constructor(...args: any[]);
+}
+
+declare module 'bezier-js/lib/bezier' {
+  export = Bezier;
+}
---
+++ src/typings/polygon-offset.d.ts
@@ -0,0 +1,7 @@
+declare class Offset {
+  public data: any;
+}
+
+declare module 'polygon-offset/dist/offset' {
+  export = Offset;
+}
